e13a0c46dab0c50c36a23c8195d5f51c8770845c
Joshua Chew <joshuagchew@gmail.com>
2021-10-22 15:03:47 -0700

    outputs a trimmed file

--- parser.py
+++ parser.py
@@ -1,6 +1,18 @@
 import sys
 import re
 
+def isNotFluff(line):
+    # return false if contains any of these 4 keywords, 
+    removeIfContains = ["Sending", "Writing", "Received", "Raising"]
+    for key in removeIfContains:
+        keyAsRegex = re.compile(key)
+        found = keyAsRegex.search(line)
+        if not found:
+            continue
+        else: 
+            return False
+    # return false if line is too short
+    return len(line) > 8
 # print ('Number of arguments:', len(sys.argv), 'arguments.')
 # print ('Argument List:', str(sys.argv))
 
@@ -23,13 +35,21 @@ if (len(filename) != 16):
 
 f = open(file, "r")
 lines = f.readlines()
-print(lines)
+f.close()
+
+date = filename[4:12]
+
+o = open(date + "-test.log", "w")
+for line in lines:
+    if isNotFluff(line):
+        o.write(line)
+
+o.close()
 
 
 # with open(file, "r") as f:
 #     lines = f.readlines()
 
-# date = filename[4:12]
 
 # with open(file, "w") as f:
 #     for line in lines:
