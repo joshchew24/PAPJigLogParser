e2015f2e66d267a9711f37c792064c8abc642056
Joshua Chew <joshuagchew@gmail.com>
2021-10-22 12:56:07 -0700

    command line argument only accepts files of format app_YYYYMMDD.log

--- parser.py
+++ parser.py
@@ -1,4 +1,5 @@
 import sys
+import re
 
 # print ('Number of arguments:', len(sys.argv), 'arguments.')
 # print ('Argument List:', str(sys.argv))
@@ -6,4 +7,16 @@ import sys
 if len(sys.argv) != 2:
      exit("Usage: \'python parser.py arg1\'\narg1 is the name of .log file to be parsed.")
 
-print("lol")
+filename = sys.argv[1]
+if (len(filename) != 16): 
+    exit("Error: filename should be of format \'app_YYYYMMDD.log\'")
+
+pattern = re.compile("app_20(1|2)\\d(0|1)\\d[0-3]\\d\\.log")
+m = pattern.match(filename)
+if not m:
+    exit("Error: filename should be of format \'app_YYYYMMDD.log\'")
+
+
+print(m)
+print(filename)
+
