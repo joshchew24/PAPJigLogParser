e321eaa864fe3cdbc7d101e2a3ff96345a5624da
Joshua Chew <joshuagchew@gmail.com>
2021-10-22 11:46:13 -0700

    began work on parsing command line arguments

--- parser.py
+++ parser.py
@@ -1,2 +1,9 @@
-L = ["asdf\n", "qwer\n", "1234\n"]
-print(L)
+import sys
+
+# print ('Number of arguments:', len(sys.argv), 'arguments.')
+# print ('Argument List:', str(sys.argv))
+
+if len(sys.argv) != 2:
+     exit("Usage: \'python parser.py arg1\'\narg1 is the name of .log file to be parsed.")
+
+print("lol")
