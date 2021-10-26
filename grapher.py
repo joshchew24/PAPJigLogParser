bedae66a88836fb54ff3b4b7cd48242a6ce3d686
Joshua Chew <joshuagchew@gmail.com>
2021-10-25 18:18:45 -0700

    added corrected data set to plot

--- grapher.py
+++ grapher.py
@@ -3,5 +3,6 @@ import pandas as pd
 
 def plot(filename):
     df = pd.read_csv(filename, names=['seconds', 'uncorrected', 'corrected'])
-    ax = df.plot.scatter(x='seconds',y='uncorrected')
+    ax1 = df.plot(kind='scatter', x='seconds', y='uncorrected',label='uncorrected', color='b')
+    ax2 = df.plot(kind='scatter', x='seconds', y='corrected', label='corrected', color='r', ax=ax1)
     plt.show()
