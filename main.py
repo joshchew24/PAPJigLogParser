5eb7af146d5c27ae8c13dba2d95cf05fec683e84
jchew <joshua.chew@kardium.com>
2021-11-05 16:51:16 -0700

    refactoring input UI

--- main.py
+++ main.py
@@ -8,29 +8,95 @@ global path
 global file
 global command
 global date
+global commands 
+commands = ["parse","graph","process","help","exit","q"]
 
 def main():
+    # global path
+    # global command
+    if len(sys.argv) == 1:
+        promptCommandInput()
+        promptFileInput()
+        executeCommand()
+    # elif len(sys.argv) != 3:
+    #     exit("Usage: \'main.py <parse/graph/process> <path>/\'\narg1 is the path/filename of .log path to be parsed.")
+    # command = sys.argv[1]
+    # path = sys.argv[2]
+    
+    # parseCommand()
+
+def promptCommandInput():
+    global command
+    command = input("Enter function... ")
+    parseCommand()
+
+    # if (command == help):
+    #     asdf = input("asdf")
+
+def promptFileInput():
+    global path
+    path = input("Enter file or directory to " + command + "... ")
+    parseFile()
+    
+
+def parseFile():
+    global file
+    global date
+    
+    pattern = re.compile("(20(1|2)\\d(0|1)\\d[0-3]\\d\\-temps\\.csv)" if (command == "graph") else "(app_20(1|2)\\d(0|1)\\d[0-3]\\d\\.log)")
+    # pattern = re.compile("(app_20(1|2)\\d(0|1)\\d[0-3]\\d\\.log)%s" %("|(20(1|2)\\d(0|1)\\d[0-3]\\d\\-temps\\.csv)" if accept_csv else ""))
+    m = pattern.search(path)
+    
+    if not m:
+        print("Error: filename should be of format \'app_YYYYMMDD.log\'. Can also graph files of format \'YYYYMMDD-temps.csv\'")
+        promptFileInput()
+
+    filename = m.group()
+    try:
+        file = open(path, "r")
+    except FileNotFoundError as e:
+        exit("Exiting: File not found. Verify it exists in the specified directory. If graphing .log files, parse them prior to graphing.")
+
+    date = filename[4:12]  
+
+def printHelp():
+    print("Parse:   reads log file and generates three new versions - trimmed, and temperature data in .log and .csv format.")
+    print("Graph:   graph a .csv file containing temperature data.")
+    print("Process: parse then graph")
+    # print("Parse: read log file and generate three new versions - trimmed, and temperature data in readable format and .csv format.")
+    # print("Parse: read log file and generate three new versions - trimmed, and temperature data in readable format and .csv format.")
+
+def getCommandStrings():
+    patternString = commands[0]
+    first = True
+
+    for s in commands:
+        if first:
+            first = False
+            continue
+        patternString += "|" + s
+
+    return patternString
+
+def parseCommand():
     global path
     global command
-    if len(sys.argv) != 3:
-        exit("Usage: \'main.py <parse/graph/process> <path>/\'\narg1 is the path/filename of .log path to be parsed.")
-    command = sys.argv[1]
-    path = sys.argv[2]
-    
-    processCommand()
 
-
-def processCommand():
-    global path
-    global command
-    
-    commandPattern = re.compile("parse|graph|process")
+    patternString = getCommandStrings()
+        
+    commandPattern = re.compile(patternString)
     commandMatch = commandPattern.search(command)
     if not commandMatch:
-        exit("\'" + command + "\' is not one of the valid commands: \'parse\', \'graph\', \'process\'")
+        print("\'" + command + "\' is not one of the valid commands: ")
+        print(*commands, sep = ', ')
+        promptCommandInput()
+    if (command == "help"): 
+        printHelp()
+        promptCommandInput()
+    if (command == "exit" or command == "q"):
+        exit("Exiting...")
 
-    readFile()
-
+def executeCommand():
     if (command == "parse"):
         parse()
     elif (command == "graph"):
@@ -40,30 +106,6 @@ def processCommand():
         graph()
 
 
-def readFile():
-    global path
-    global file
-    global date
-    accept_csv = (command == "graph")
-    
-    pattern = re.compile("(app_20(1|2)\\d(0|1)\\d[0-3]\\d\\.log)%s" %("|(20(1|2)\\d(0|1)\\d[0-3]\\d\\-temps\\.csv)" if accept_csv else ""))
-    m = pattern.search(path)
-
-    if not m:
-        exit("Error: filename should be of format \'app_YYYYMMDD.log\'. Can also graph files of format \'YYYYMMDD-temps.csv\'")
-
-    filename = m.group()
-
-    # useless check, matched regex will guarantee length.
-    # if (len(filename) != 16): 
-    #     exit("Error: filename should be of format \'app_YYYYMMDD.log\'")
-
-    try:
-        file = open(path, "r")
-    except FileNotFoundError as e:
-        quit("Error: File not found. Verify it exists in the specified directory. If graphing .log files, parse them prior to graphing.")
-
-    date = filename[4:12]
 
 
 def parse():
--- readme.md
+++ readme.md
@@ -4,6 +4,9 @@
 - improve parser code readability
 - command line: multiple functions, can process entire folder or just single files
 - graphing: more visual clarity, outputs image file OR can open in graphing viewer?
+- chagne output folder to same directory as input 
+- outputs new folder for each date
+
 
 
 ## Usage:
